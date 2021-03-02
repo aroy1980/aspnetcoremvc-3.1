515a8b744823c243d60a69b94d58b450041015bd
aroy1980 <aroy13@gmail.com>
2021-03-02 16:59:44 +0600

    02-03-21

--- WebGentle.BookStore/WebGentle.BookStore/Startup.cs
+++ WebGentle.BookStore/WebGentle.BookStore/Startup.cs
@@ -2,9 +2,11 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -17,6 +19,7 @@ namespace WebGentle.BookStore
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddRazorPages().AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -32,6 +35,12 @@ namespace WebGentle.BookStore
             //    await context.Response.WriteAsync("dfgsdfg");
             //});
             app.UseStaticFiles();
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "MyStaticFiles")),
+                RequestPath = "/MyStaticFiles"
+            });
+               
             app.UseRouting();
             app.UseEndpoints(endpoints =>
             {
